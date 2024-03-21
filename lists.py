9404ac5af064e811189b6cb1b5453ec95908d8f4
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-03-21 03:19:46 +0200

    updated list comps

--- lists.py
+++ lists.py
@@ -1,3 +1,19 @@
 sports = ["soccer", "rugby", "skating", "swimming"]
 
-print(sports[1:2])
+print(sports[0])
+
+# list comprehension
+
+nums = [1, 4, 78, 30, 15]
+doubled = []
+
+for num in nums:
+    doubled.append(num * 2)
+
+    print(doubled)
+
+#List comprehensions to solve these types of problems in one line.
+
+nums = [1, 4, 78, 30, 15]
+doubled = [num * 2 for num in nums]
+print(doubled)
